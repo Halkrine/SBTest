c5d2b14c2fbc7bdf423c7ea6f3f5c659ba32abc7
Halkrine <halkrine@gmail.com>
2021-06-23 22:50:08 +0900

    added logback logger

--- src/main/java/com/sbtest/service/impl/TestServiceImpl.java
+++ src/main/java/com/sbtest/service/impl/TestServiceImpl.java
@@ -3,6 +3,8 @@ package com.sbtest.service.impl;
 import com.sbtest.dto.TestDto;
 import com.sbtest.mapper.TestMapper;
 import com.sbtest.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +13,16 @@ import java.util.Map;
 
 @Service
 public class TestServiceImpl implements TestService {
-
+    private final Logger log = LoggerFactory.getLogger(TestServiceImpl.class);
 
     @Autowired
     TestMapper mapper;
 
     public List<TestDto> getList(){
         List<TestDto> rtnList = mapper.getList();
-
+        log.info("info TEST");
+        log.debug("debug TEST");
+        log.debug(rtnList.toString());
         return rtnList;
     }
 }
